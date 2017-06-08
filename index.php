5ceb7ff53917c909f6363efa436ec83d10dcbdd6
cetecom <cetecom@LC08L1215>
2017-06-08 12:06:27 -0400

    jquery

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <script src="js/jquery-3.2.1.min.js" type="text/javascript"></script>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form>
+            <input type="text" name="nomusuario" id="nomusuario">
+            <input type="password" name="clave" id="clave">
+            <input type="button" onclick="" value="enviar">
+            
+            
+     </form>
+        
+    </body>
+</html>
